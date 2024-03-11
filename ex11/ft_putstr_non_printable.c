6636ac9270cf313ce10d6aa30cb15439998b9b88
marciochamma <82783953+marciochamma@users.noreply.github.com>
2024-03-11 00:05:31 -0300

    Update ft_putstr_non_printable.c

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -69,25 +69,3 @@ void	ft_putstr_non_printable(char *str)
 // 	ft_putstr_non_printable(str);
 // 	return (0);
 // }
-
-#include <stdio.h>
-#include <unistd.h>
-
-int	main(void)
-{
-	char src[0xFF + 10];
-	int	i;
-
-	i = 1;
-	while (i <= 0xFF)
-	{
-		src[i - 1] = i;
-		i++;
-	}
-	src[i - 1] = 0;
-
-	ft_putstr_non_printable("Coucou\ntu vas bien ?");
-	write(1, "\n", 1);
-	ft_putstr_non_printable(src);
-	write(1, "\n", 1);
-}
